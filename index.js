a1d34d0ccf7b12ffda05eb4846d04f2238a797ac
Frhnnn <kiblatgt@gmail.com>
2024-12-30 11:58:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,19 +16,10 @@ import produkRoutes from './routes/produk.js';
 import responsesRoutes from './routes/responses.js';
 import usersRoutes from './routes/users.js';
 
-const apiKeyMiddleware = (req, res, next) => {
-  const key = req.path.split('/')[1];
-  req.url = req.url.replace(`/${key}`, ''); 
-  if (!key) return res.status(401).json({ message: 'API Key tidak diberikan.' });
-  if (key !== process.env.API_KEY) return res.status(401).json({ message: 'API Key tidak valid.' });
-  next();
-};
-
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(apiKeyMiddleware); 
 
-const availableRoutes = [
+const all = [
   '/buttons',
   '/datadonate',
   '/kategori',
@@ -40,26 +31,48 @@ const availableRoutes = [
 
 app.get('/', async (req, res) => {
   try {
-    const serverInfo = {
-      hostname: os.hostname(),
-      platform: os.platform(),
-      architecture: os.arch(),
-      cpuCores: os.cpus().length,
-      totalMemory: os.totalmem(),
-      freeMemory: os.freemem(),
-      uptime: os.uptime(),
-    };
+    const totalMem = os.totalmem();
+    const freeMem = os.freemem();
+    const usedMem = totalMem - freeMem;
+    const uptime = os.uptime();
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const hours = Math.floor((uptime % (60 * 60 * 24)) / (60 * 60));
+    const minutes = Math.floor((uptime % (60 * 60)) / 60);
+
+    const serverInfo = `
+RAM: ${formatBytes(usedMem)} / ${formatBytes(totalMem)}
+FreeRAM: ${formatBytes(freeMem)}
+
+Model: ${os.cpus()[0].model}
+Arsitektur: ${os.arch()}
+Inti: ${os.cpus().length}
+Kecepatan: ${(os.cpus()[0].speed / 1000).toFixed(2)} GHz
+
+Platform: ${os.platform()}
+Versi: ${os.release()}
+Uptime: ${days} hari ${hours} jam ${minutes} menit
+`.trim();
+
     res.json({
       status: "Database nya aktif hann :3",
-      pesan: "Hacker jangan menyerang !!!",
-      serverInfo: serverInfo,
-      availableRoutes,
+      pesan: "Hacker jangan menyerang !",
+      server: serverInfo,
+      all,
     });
   } catch (error) {
     res.status(500).json({ message: "Gagal mengambil data.", error: error.message });
   }
 });
 
+const apiKeyMiddleware = (req, res, next) => {
+  const key = req.path.split('/')[1];
+  req.url = req.url.replace(`/${key}`, ''); 
+  if (!key) return res.status(401).json({ message: 'API Key tidak diberikan.' });
+  if (key !== process.env.API_KEY) return res.status(401).json({ message: 'API Key tidak valid.' });
+  next();
+};
+
+app.use(apiKeyMiddleware); 
 app.use('/buttons', buttonRoutes);
 app.use('/datadonate', donorDataRoutes);
 app.use('/kategori', kategoriRoutes);
@@ -75,4 +88,15 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-  
+
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
