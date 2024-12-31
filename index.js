a5fbc816366c4cf738fc11e04aeb3a854b8a3904
Frhnnn <kiblatgt@gmail.com>
2025-01-01 00:23:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -68,11 +68,13 @@ const allowedOrigins = [
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin) || req.method === 'GET') {
+
+  if (allowedOrigins.includes(origin) || !origin || req.method === 'GET') { 
     res.setHeader('Access-Control-Allow-Origin', origin || '*'); 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-api-key');
   }
+
   next();
 });
 
