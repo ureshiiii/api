afef8f68d67ee32bd68ebc73619915ce5b1bce0f
Frhnnn <kiblatgt@gmail.com>
2024-12-31 23:46:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,23 @@ const apiKeyMiddleware = (req, res, next) => {
   }
 };
 
+const allowedOrigins = [
+  'www.ureshii.my.id', 
+  'ureshii.my.id', 
+  'api.ureshii.my.id', 
+  'list-store.ureshii.my.id'
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin) || req.method === 'GET') {
+    res.setHeader('Access-Control-Allow-Origin', origin || '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-api-key');
+  }
+  next();
+});
+
 app.get('/server-info', (req, res) => {
   try {
     const totalMem = os.totalmem();
