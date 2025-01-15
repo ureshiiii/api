86da6e4fd379864c975f3740617c1f42baefcfd2
Frhnnn <kiblatgt@gmail.com>
2025-01-15 08:10:12 +0700

    Update jarak.js

--- routes-fitur/tools/jarak.js
+++ routes-fitur/tools/jarak.js
@@ -6,40 +6,18 @@ import uploadImage from '../../lib/uploadImage.js';
 const router = express.Router();
 
 async function jarak(dari, ke) {
-  try {
-    const url = `https://www.google.com/search?q=${encodeURIComponent(`jarak ${dari} ke ${ke}`)}&hl=id`;
-    const response = await axios.get(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-      }
-    });
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const hasil = {};
-
-    const img = html.split("var s=\'")?.[1]?.split("\'")?.[0];
-    if (/^data:.*?\/.*?;base64,/i.test(img)) {
+	var html = (await axios(`https://www.google.com/search?q=${encodeURIComponent('jarak ' + dari + ' ke ' + ke)}&hl=id`)).data
+	var $ = cheerio.load(html), obj = {}
+	if (/^data:.*?\/.*?;base64,/i.test(img)) {
       const imageBuffer = Buffer.from(img.split`,`[1], 'base64');
-      hasil.img = await uploadImage(imageBuffer);
+      obj.img = await uploadImage(imageBuffer);
     } else {
       const imgElement = $('g-img > img');
       const imgUrl = imgElement.attr('data-src');
-      if (imgUrl) hasil.img = imgUrl;
+      if (imgUrl) obj.img = imgUrl;
     }
-
-    hasil.jarak = $('div.BNeawe.deIvCb.AP7Wnd').first().text().trim();
-    hasil.waktu_tempuh = $('div.BNeawe.deIvCb.AP7Wnd').eq(1).text().trim();
-    hasil.rute = $('div.BNeawe.uEec3.AP7Wnd').text().trim()
-
-    if (!hasil.jarak) {
-        throw new Error("Tidak dapat menemukan informasi jarak.");
-    }
-    
-    return hasil;
-  } catch (error) {
-    console.error('Error saat mengambil/memproses data jarak:', error);
-    throw new Error(`Gagal mengambil data jarak: ${error.message}`);
-  }
+	obj.desc = $('div.BNeawe.deIvCb.AP7Wnd').text()?.trim()
+	return obj
 }
 
 router.get('/', async (req, res) => {
@@ -52,9 +30,12 @@ router.get('/', async (req, res) => {
   }
 
   try {
-    const dataJarak = await jarak(dari, ke);
+    const data = await jarak(dari, ke);
     res.status(200).json({
-      data: dataJarak
+      data: {
+        img: data.img,
+        jarak: data.desc,
+      }
     });
   } catch (error) {
     res.status(500).json({
