96e9d5bf1eb695992209df9a6a5ad90288860bc3
Frhnnn <kiblatgt@gmail.com>
2025-01-15 07:12:20 +0700

    Update jarak.js

--- routes-fitur/tools/jarak.js
+++ routes-fitur/tools/jarak.js
@@ -5,7 +5,7 @@ import { uploadImage } from '../../lib/uploadImage.js';
 
 const router = express.Router();
 
-async function hitungJarak(dari, ke) {
+async function jarak(dari, ke) {
   try {
     const url = `https://www.google.com/search?q=${encodeURIComponent(`jarak ${dari} ke ${ke}`)}&hl=id`;
     const response = await axios.get(url, {
@@ -13,31 +13,28 @@ async function hitungJarak(dari, ke) {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
       }
     });
-    const $ = cheerio.load(response.data);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const hasil = {};
 
-    const hasil = {
-      jarak: $('div.BNeawe.deIvCb.AP7Wnd').first().text().trim(),
-      waktu_tempuh: $('div.BNeawe.deIvCb.AP7Wnd').eq(1).text().trim(),
-      rute: $('div.BNeawe.uEec3.AP7Wnd').text().trim(),
-      img: null
-    };
-
-    const imgElement = $('g-img > img');
-    if (imgElement.length) {
-        const base64Img = imgElement.attr('src');
-        if (base64Img && /^data:image\/(png|jpe?g);base64,/i.test(base64Img)) {
-            const imageBuffer = Buffer.from(base64Img.split(',')[1], 'base64');
-            hasil.img = await uploadImage(imageBuffer);
-        } else {
-            const imgUrl = imgElement.attr('data-src');
-            if (imgUrl) hasil.img = imgUrl;
-        }
+    const img = html.split("var s=\'")?.[1]?.split("\'")?.[0];
+    if (/^data:.*?\/.*?;base64,/i.test(img)) {
+      const imageBuffer = Buffer.from(img.split`,`[1], 'base64');
+      hasil.img = await uploadImage(imageBuffer);
+    } else {
+      const imgElement = $('g-img > img');
+      const imgUrl = imgElement.attr('data-src');
+      if (imgUrl) hasil.img = imgUrl;
     }
+
+    hasil.jarak = $('div.BNeawe.deIvCb.AP7Wnd').first().text().trim();
+    hasil.waktu_tempuh = $('div.BNeawe.deIvCb.AP7Wnd').eq(1).text().trim();
+    hasil.rute = $('div.BNeawe.uEec3.AP7Wnd').text().trim()
 
     if (!hasil.jarak) {
         throw new Error("Tidak dapat menemukan informasi jarak.");
     }
-
+    
     return hasil;
   } catch (error) {
     console.error('Error saat mengambil/memproses data jarak:', error);
@@ -50,17 +47,20 @@ router.get('/', async (req, res) => {
 
   if (!dari || !ke) {
     return res.status(400).json({
+      status: 'gagal',
       pesan: 'Parameter `dari` dan `ke` harus diisi!'
     });
   }
 
   try {
-    const dataJarak = await hitungJarak(dari, ke);
+    const dataJarak = await jarak(dari, ke);
     res.status(200).json({
+      status: 'sukses',
       data: dataJarak
     });
   } catch (error) {
     res.status(500).json({
+      status: 'gagal',
       pesan: error.message
     });
   }
