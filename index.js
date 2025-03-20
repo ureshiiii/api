0ccc7bbb18aa04a404f77c673e7076d7d0417b3d
Frhnnn <kiblatgt@gmail.com>
2025-03-21 03:28:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(helmet());
 
 const limiter = rateLimit({
