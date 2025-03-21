1bbc620a552efb31933efedf4983ad0c6f0ef0b3
Frhnnn <kiblatgt@gmail.com>
2025-03-21 22:42:26 +0700

    Delete public/js directory

--- public/js/jquery.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $(".faq-question h5").click(function() {
-    $(this).parent().toggleClass('active');
-    $(this).next('.faq-answer').slideToggle('fast');
-  });
-});
